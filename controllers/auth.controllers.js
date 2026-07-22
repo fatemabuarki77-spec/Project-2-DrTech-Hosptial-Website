e9b39cb87f3246a934b7da10993eae2ce25b425a
fatemabuarki77-spec <Fatemabuarki77@gmail.com>
2026-07-22 15:32:09 +0300

    CSS EDIT-1

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
+const User = require("../models/User.js");
 const Profile = require("../models/profile.js");
 const bcrypt = require("bcrypt");
 
